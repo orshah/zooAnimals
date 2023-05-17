e14eea46a3b8ef729eb4f8054a4f6fe10122c652
orshah <orshpov@gmail.com>
2023-05-17 16:56:29 -0500

    first commit

---
+++ zooAnimals.js
@@ -0,0 +1,49 @@
+// Copy past this multi line comment to new file called zooAnimals.js
+
+// In the file do the following:
+
+// Declare a new variable named zooLion and give it the name of Alex.
+
+// Declare a new variable named zooZebra and give it the name Marty.
+
+// Console.log the zooLion variable.
+
+// Console.log the zooZebra variable.
+
+// Console.log the text "A lion's name is: " and the zooLion variable.
+
+// Console.log the text "A zebra's name is: " and the zooZebra variable.
+
+// Declare another variable and name it lionSound. Assign the string of "ROAAAARRR" to it.
+
+// Declare another variable and name it zebraSound. Assign the string of "HEHEHEHE" to it.
+
+// Console.log the variable zooLion, then the string "says", then the variable lionSound.
+
+// Console.log the variable zooZebra, then the string "says", then the variable zebraSound.
+
+// Reassign the value stored in lionSound to the string "MEAAAOOOW".
+
+// Console.log the variable zooZebra, then the string "now says", then the variable lionSound.
+
+// Commit the file to your github.
+// */
+
+let zooLion = "Alex";
+let zooZebra = "Marty";
+
+console.log(zooLion);
+console.log(zooZebra);
+
+console.log(`A lion's name is: ${zooLion}`);
+console.log(`A zebra's name is: ${zooZebra}`);
+
+let lionSound = "ROAAAARRR";
+let zebraSound = "HEHEHEHE";
+
+console.log(`${zooLion} says ${lionSound}`);
+console.log(`${zooZebra} says ${zebraSound}`);
+
+lionSound = "MEAAAOOOW";
+
+console.log(`${zooZebra} now says ${lionSound}`);
